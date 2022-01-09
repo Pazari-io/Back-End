9bbf5409a9bd95fa9417566a9db3f0cab62f13ae
0xsha <me@0xsha.io>
2022-01-09 14:38:19 +0700

    cleanup, add original file download, more checks , enviroment variables and config files

---
+++ internal/command.go
@@ -0,0 +1,37 @@
+package internal
+
+import (
+	"context"
+	"errors"
+	"os/exec"
+	"time"
+)
+
+func ExecuteCommand(command string, timeOut time.Duration, args ...string) (string, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeOut*time.Second)
+	defer cancel() // The cancel should be deferred so resources are cleaned up
+
+	// log.Println(command, args)
+	// return "", nil
+	// Create the command with our context
+	cmd := exec.CommandContext(ctx, command, args...)
+
+	// This time we can simply use Output() to get the result.
+	out, err := cmd.Output()
+
+	if ctx.Err() == context.DeadlineExceeded {
+		//log.Println("Command timed out")
+		return "", errors.New("command timed out")
+	}
+
+	if err != nil {
+
+		return "", err
+
+	}
+
+	// If there's no context error, we know the command completed (or errored).
+	return string(out), nil
+
+}
---
+++ internal/hashing.go
@@ -0,0 +1,25 @@
+package internal
+
+import (
+	"crypto/rand"
+	"crypto/sha512"
+	"encoding/hex"
+)
+
+func ShaHash() string {
+
+	randomString := randomString(40)
+	sha512Sum := sha512.Sum512_256([]byte(randomString))
+	return hex.EncodeToString(sha512Sum[:])
+
+}
+
+func randomString(n int) string {
+	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, n)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
+	}
+	return string(bytes)
+}
