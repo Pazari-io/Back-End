1d3ef57e47cc89f69e48812d46d64b075781a364
0xsha <me@0xsha.io>
2022-01-10 18:54:41 +0700

    SSL on Prod

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.23.0
 	github.com/gofiber/keyauth/v2 v2.1.14
 	github.com/joho/godotenv v1.4.0
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 	gorm.io/datatypes v1.0.5
 	gorm.io/driver/sqlite v1.2.6
 	gorm.io/gorm v1.22.4
@@ -21,7 +22,9 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.31.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20210510120150-4163338589ed // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
 	gorm.io/driver/mysql v1.2.2 // indirect
 	gorm.io/driver/postgres v1.2.3 // indirect
 	gorm.io/driver/sqlserver v1.2.1 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"crypto/tls"
+	"log"
+
 	"github.com/Pazari-io/Back-End/database"
 	"github.com/Pazari-io/Back-End/internal"
 	"github.com/Pazari-io/Back-End/routes"
 	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
@@ -12,6 +16,35 @@ func main() {
 	database.InitDB()
 	app := fiber.New()
 
+	if internal.GetKey("PRODUCTION") == "true" {
+
+		// Certificate manager
+		m := &autocert.Manager{
+			Prompt: autocert.AcceptTOS,
+			// Replace with your domain
+			HostPolicy: autocert.HostWhitelist("api.pazari.io"),
+			// Folder to store the certificates
+			Cache: autocert.DirCache("./certs"),
+		}
+
+		// TLS Config
+		cfg := &tls.Config{
+			// Get Certificate from Let's Encrypt
+			GetCertificate: m.GetCertificate,
+			NextProtos: []string{
+				"http/1.1", "acme-tls/1",
+			},
+		}
+		ln, err := tls.Listen("tcp", "0.0.0.0:443", cfg)
+		if err != nil {
+			panic(err)
+		}
+
+		log.Fatal(app.Listener(ln))
+
+	}
+
+	// set up routes
 	routes.InitRoutes(app)
 	app.Listen("0.0.0.0:" + internal.GetKey("PORT"))
 }
